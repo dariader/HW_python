f91af7078e3b3369a9beb5f32c21a22f16546534
dariader <30576713+dariader@users.noreply.github.com>
2018-11-10 16:05:10 +0300

    Update Rna_Dna.py

--- Rna_Dna.py
+++ Rna_Dna.py
@@ -1,89 +1,44 @@
 class Rna: #define  class 
     def __init__(self, acid):
-        self.acid = list(acid)
         
-        if  'C' and 'A' and 'G' and 'U' not in list(self.acid):
+        assert isinstance(acid, str)
+        self.acid = acid.upper()
+        
+        if  'C' and 'A' and 'G' and 'U' not in self.acid:
             raise 'That is nor RNA sequence, please, check the data'
         else:
             pass
         
-    def gc(self):
-        j = 0
-        for i in self.acid:
-            if i == 'G' or i == 'C':
-                j = j + 1
-            else:
-                print('0','%')
-        return (j/len(self.acid)*100,'%')
+    def gc(self): 
+        return (self.acid.count('G') + self.acid.count('C')) / len(self.acid)
     
     def reverse_complement(self):
-        j = []
-        for i in self.acid: # not really good dictionary. 
-            if i == 'G':
-                j += 'C'
-            else:
-                if i == 'C':
-                    j += 'G'
-                else: 
-                    if i == 'A':
-                        j += 'U'
-                    else:
-                        if i == 'U':
-                            j += 'A'
-        return j[::-1]   
+        return self.acid.translate(str.maketrans('AUGC', 'UACG'))
 
 class Dna: #define  class 
     def __init__(self, acid):
-        self.acid = list(acid)
         
-        if  'C' and 'A' and 'G' and 'T' not in list(self.acid):
-            raise 'That is nor RNA sequence, please, check the data'
+        assert isinstance(acid, str)
+        self.acid = acid.upper()
+        
+        if  'C' and 'A' and 'G' and 'T' not in self.acid:
+            raise 'That is nor DNA sequence, please, check the data'
         else:
             pass
         
-    def gc(self):
-        j = 0
-        for i in self.acid:
-            if i == 'G' or i == 'C':
-                j = j + 1
-            else:
-                print('0','%')
-        return (j/len(self.acid)*100,'%')
+    def gc(self):  
+        return (self.acid.count('G') + self.acid.count('C')) / len(self.acid)
     
     def reverse_complement(self):
-        j = []
-        for i in self.acid:
-            if i == 'G':
-                j += 'C'
-            else:
-                if i == 'C':
-                    j += 'G'
-                else: 
-                    if i == 'A':
-                        j += 'T'
-                    else:
-                        if i == 'T':
-                            j += 'A'
-        return j[::-1]   
+        return self.acid.translate(str.maketrans('ATGC', 'TACG'))
     
     
-    def transcribe(self): 
-        j = []
-        for i in self.acid:
-            if i == 'G':
-                j += 'C'
-            else:
-                if i == 'C':
-                    j += 'G'
-                else: 
-                    if i == 'A':
-                        j += 'U'
-                    else:
-                        if i == 'T':
-                            j += 'A'
-        return Rna(j)
+    def transcribe(self):
+        transcript = self.reverse_complement().translate(str.maketrans('T', 'U'))
+        return Rna(transcript)
 
 
 first = Dna('ATGC')
-first.reverse_complement()
-first.transcribe()
+print(first.reverse_complement())
+print(first.gc())
+print(first.transcribe())
