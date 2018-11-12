6df6ee8371e3f053d4c973a41f5e7b8b9a1844f0
dariader <30576713+dariader@users.noreply.github.com>
2018-11-12 16:42:29 +0300

    Update Rna_Dna.py

--- Rna_Dna.py
+++ Rna_Dna.py
@@ -2,18 +2,21 @@ class Rna: #define  class
     def __init__(self, acid):
         
         assert isinstance(acid, str)
-        
+        self.reverse_complement = ""
         self.acid = acid.upper()
         
-        if 'C' and 'A' and 'G' and 'U' not in self.acid:
+        if not filter(lambda m: m not in 'AUGC', self.acid):
             raise 'TypeError01: Not RNA sequence, please, check the data'
         else:
+            print(acid, '5-3 RNA is your input')
             pass
+        
         
     def gc(self): 
         return (self.acid.count('G') + self.acid.count('C')) / len(self.acid)
     
     def reverse_complement(self):
+        
         return self.acid.translate(str.maketrans('AUGC', 'UACG'))[::-1]
 
 class Dna(Rna): #define  class 
@@ -21,24 +24,28 @@ class Dna(Rna): #define  class
         
         assert isinstance(acid, str)
         self.acid = acid.upper()
-        
-        if 'C' and 'A' and 'G' and 'T' not in self.acid:
+            
+        if not filter(lambda m: m not in 'ATGC', self.acid):
             raise 'TypeError01: Not DNA sequence, please, check the data'
         else:
+            print(acid, '5-3 DNA is your input')
             pass
+        
     def reverse_complement(self):
         return self.acid.translate(str.maketrans('ATGC', 'TACG'))[::-1]
+        
     
-     
     def transcribe(self):
-        transcript = Rna(self.acid.translate(str.maketrans('T', 'U')))
-        print(transcript.acid) #to check the result
-        return transcript #to return an object
+        transcript = Rna(self.acid.translate(str.maketrans('T', 'U'))) 
+        
+        
+        return (transcript.acid)         #returns 5' - 3' RNA
     
-    ###test data
-first = Dna(('AGATACACA'))
-print(first.reverse_complement())
-print(first.gc())
-second = first.transcribe()
-print(second)
-second.reverse_complement()
+    
+ ############# Usage examples
+    
+input_dna = Dna(('GGGG'))
+print(input_dna.reverse_complement())
+print(input_dna.gc())
+output_rna = input_dna.transcribe()
+print(output_rna)
